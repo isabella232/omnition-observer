1913cd97d2d486447c62ac71a44aefa60ac0e73d
Owais Lone <hello@owaislone.org>
2018-08-03 04:58:52 +0530

    Add basic TLS support and better config generation
    Added basic support for TLS. Also added a program that auto-generates
    envoy config from CLI/ENV vars. The program can evolve to become the
    main CLI interface to omnition observer. It can also replace the sidecar
    entry point and manage envoy itself. Can also deal with hot-restarts on
    config change, etc.
    
    Issue: A0-152

---
+++ observer/pkg/options/options.go
@@ -0,0 +1,65 @@
+package options
+
+import (
+	"github.com/ansel1/merry"
+)
+
+// Tracing system identifiers
+const (
+	_ = iota
+	ZIPKIN
+	JAEGER
+)
+
+// Config ..
+type Options struct {
+	TLSEnabled bool
+	TLSCACert  string
+	TLSCert    string
+	TLSKey     string
+
+	AdminPort    int
+	AdminLogPath string
+	IngressPort  int
+	EgressPort   int
+
+	TracingDriver  string
+	TracingAddress string
+	TracingPort    int
+}
+
+func New(
+	IngressPort int,
+	EgressPort int,
+	TracingDriver string,
+	TracingAddress string,
+	TracingPort int,
+	TLSEnabled bool,
+	TLSCACert string,
+	TLSCert string,
+	TLSKey string,
+	AdminPort int,
+	AdminLogPath string,
+) (Options, error) {
+	if TLSEnabled {
+		if TLSCert == "" || TLSKey == "" {
+			return Options{}, merry.New("TLS cannot be enabled without certificate cert and key")
+		}
+	}
+	return Options{
+		IngressPort: IngressPort,
+		EgressPort:  EgressPort,
+
+		TracingDriver:  TracingDriver,
+		TracingAddress: TracingAddress,
+		TracingPort:    TracingPort,
+
+		TLSEnabled: TLSEnabled,
+		TLSCert:    TLSCert,
+		TLSCACert:  TLSCACert,
+		TLSKey:     TLSKey,
+
+		AdminPort:    AdminPort,
+		AdminLogPath: AdminLogPath,
+	}, nil
+}
